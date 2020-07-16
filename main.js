29977e549d8fefec66907112641ac1210159434b
Nguyen Le <nle20959@gmail.com>
2020-07-17 01:19:59 +0700

    version-2

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let page = 1
 const apiKey = `fb79e1dd73084533b72be1bc6d99e5c0`
 let currentCategory = "General";
 let keyWord = "";
+let sources = {};
+let currentList = [];
 
 const callApi = async () => {
     console.log(currentCategory)
@@ -15,33 +17,7 @@ const callApi = async () => {
     console.log(newsList)
 }
 
-const render = (list) => {
-    //use moment js to show publishedAT data
-    let newsHTML = list.map(item => {
-        return `<div class="style_news"><div class="card text-justify" style="width: 50rem;">
-        <img src="${item.urlToImage}" class="card-img-top"  alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title style_title"><a href = "${item.url}">${item.title}</a></h5>
-          <p class="card-text">${item.content}</p>
-        </div>
-        <div>
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item">${moment(item.publishedAt).fromNow()}</li>
-        <li class="list-group-item "><strong>Source</strong>: ${item.source.name}</li>
-        <li class="list-group-item "><strong>Author</strong>: ${item.author}</li>
-        </ul>
-        </div>
-        <div class="card-body">
-        <a href="${item.url}" class="card-link btn btn-primary">Read more</a>
-        </div>
-        </div>
-        </div>`
-    })
-    document.getElementById("newsListArea").innerHTML = newsHTML
-}
-
 //able to search by catagory
-
 let myArrayCategory = ['Business', 'Entertainment', 'General', 'Health', 'Science', 'Sports', 'Technology']
 
 function newButton() {
@@ -64,7 +40,6 @@ async function searchByCategory(index) {
 }
 
 //by keyword
-
 async function searchByKeyWord() {
     keyWord = document.getElementById("keyWord").value;
     url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}&q=${keyWord}&category=${currentCategory}`;
@@ -75,12 +50,6 @@ async function searchByKeyWord() {
     console.log(url)
 }
 
-
-// able to load more articles
-const viewMore = () => {
-    page++
-    callApi()
-}
 
 //able to search by source
 // async function searchBySource() {
@@ -93,33 +62,83 @@ const viewMore = () => {
 //     render(newList)
 //     console.log(url)
 // }
-const renderSource = () => {
-    let sources = newsList.map((item) => item.source.name);
-    let result = {};
-    for (let i = 0; i < sources.length; i++) {
-      if (sources[i] in result) {
-        result[sources[i]]++;
-      } else {
-        result[sources[i]] = 1;
-      }
+
+//get all sources
+// async function getAllSources() {
+//   url = `https://newsapi.org/v2/sources?apiKey=${apiKey}`;
+//   let data = await fetch(url)
+//   let result = await data.json()
+//   console.log("ahihi")
+//   console.log(result)
+// }
+// getAllSources()
+
+function newListToSources(){
+  let duplicatedSources = newsList.map((item) => item.source.name);
+  console.log(sources)
+  sources = {};
+  for (let i = 0; i < duplicatedSources.length; i++) {
+    if (duplicatedSources[i] in sources) {
+      sources[duplicatedSources[i]]++;
+    } else {
+      sources[duplicatedSources[i]] = 1;
     }
-    console.log("result is",result)
-    
-    let keys = Object.keys(result);
-    let sourceHTML = keys.map((key) =>
-          `<input type="checkbox" onchange="searchBySource('${key}')">${key}:${result[key]}`
+  }
+}
+
+
+const renderSource = () => {
+  newListToSources();
+console.log("result is" + JSON.stringify(sources))
+    let sourceNames = Object.keys(sources);
+    let sourceHTML = sourceNames.map((sourceName) =>
+          `<input id=${sourceName} type="checkbox" onchange="searchBySource('${sourceName}')">${sourceName}:${sources[sourceName]}`
       )
       .join("");
   
     document.getElementById("sourceArea").innerHTML = sourceHTML;
   };
-
-const searchBySource = (source) => {
-    console.log("source?:", source);
-    let filteredList = newsList.filter((item) => item.source.name === source);
-    render(filteredList);
-  };
   
+
+const searchBySource = (sourceName) => {
+
+    console.log("source?:", sourceName);
+    let filteredList = newsList.filter((item) => item.source.name === sourceName);
+    render(filteredList);
+    document.getElementById(sourceName).checked = true;
+  };
+
+// able to load more articles
+const viewMore = () => {
+  page++
+  callApi()
+}
+  
+  const render = (list) => {
+    //use moment js to show publishedAT data
+    let newsHTML = list.map(item => {
+        return `<div class="style_news"><div class="card text-justify" style="width: 50rem;">
+        <img src="${item.urlToImage}" class="card-img-top"  alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title style_title"><a href = "${item.url}">${item.title}</a></h5>
+          <p class="card-text">${item.content}</p>
+        </div>
+        <div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">${moment(item.publishedAt).fromNow()}</li>
+        <li class="list-group-item "><strong>Source</strong>: ${item.source.name}</li>
+        <li class="list-group-item "><strong>Author</strong>: ${item.author}</li>
+        </ul>
+        </div>
+        <div class="card-body">
+        <a href="${item.url}" class="card-link btn btn-primary">Read more</a>
+        </div>
+        </div>
+        </div>`
+    })
+    document.getElementById("newsListArea").innerHTML = newsHTML
+    renderSource();
+}
 
 
 callApi();
